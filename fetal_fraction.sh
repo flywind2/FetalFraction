0b6038fb4afa27a94d2830f3ce2b22b0b641c6b4
Priit Paluoja <priitpaluoja@gmail.com>
2019-05-10 13:23:12 +0300

    Update fetal_fraction.sh

--- fetal_fraction.sh
+++ fetal_fraction.sh
@@ -11,15 +11,12 @@
 # Number of CPU cores per task
 #SBATCH --cpus-per-task=10
 
-
-#
 # Usage:
 # 1. Provide system with R and Anaconda with source "ff" which contains bowtie2 and samtools. (conda install bowtie2 and conda install samtools)
 # 2. Download and modify seqFF to accept piped sam file.
 # 3. Download bowtie2 hg19 pre-built index file and update bash script with corresponding location
 # 4. Fill file "locations" with folder paths to samples. All sample X files must be in one folder. Locations can contain any number of samples.
 # 5. Run the script. Last tested on Rocket cluster (rocket.hpc.ut.ee) on 26.05.2018.
-
 
 module load R-3.2.0
 module load python-3.6.3
